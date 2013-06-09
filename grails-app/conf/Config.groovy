956484ce19ec79446fc30d39f857ce2d36f5c677
Igor Artamonov <igor@artamonov.ru>
2013-06-09 19:54:40 +0700

    GA for docs

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -28,3 +28,18 @@ grails.views.default.codec="none" // none, html, base64
 grails.views.gsp.encoding="UTF-8"
 
 
+grails.doc.footer = """
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-55990-12']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+"""
